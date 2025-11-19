2715c40b712e4a634d5175eaaa54e1681b506274
brow51wss <brow5187@gmail.com>
2025-11-19 10:11:51 +0800

    Update .gitignore and remove build artifacts from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"j7ccsdcoqQunMTK4N22SZ7w7eMHYZsaNAO50wIlUAV4=\"\n}"
--- .next/dev/static/chunks/Desktop_fames-hawaii_app_page_tsx_07a3998e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_fames-hawaii_04627d70._.js",
-  "static/chunks/99af9__pnpm_de6ea505._.js"
-],
-    source: "dynamic"
-});
